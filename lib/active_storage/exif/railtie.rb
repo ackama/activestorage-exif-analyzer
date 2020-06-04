d08c667c5640d12651efed186d8e43ca18e4a177
Josh McArthur <joshua.mcarthur@gmail.com>
2020-06-05 09:54:38 +1200

    Add missing class-level documentation to ActiveStorage::Exif::Railtie

--- lib/active_storage/exif/railtie.rb
+++ lib/active_storage/exif/railtie.rb
@@ -2,6 +2,9 @@
 
 module ActiveStorage
   module Exif
+    ##
+    # Adds the EXIF analyser to the list of ActiveStorage analyzers
+    # when the Rails app is configured
     class Railtie < Rails::Railtie
       config.active_storage.analyzers.prepend ActiveStorage::Exif::Analyzer
     end
