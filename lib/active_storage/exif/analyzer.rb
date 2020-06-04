70ed77b730f4b915f667461d5e056548ce56c25e
Josh McArthur <joshua.mcarthur@gmail.com>
2020-06-05 09:54:17 +1200

    Add more documentation about how we use read_image

--- lib/active_storage/exif/analyzer.rb
+++ lib/active_storage/exif/analyzer.rb
@@ -8,7 +8,13 @@ module ActiveStorage
     ##
     # Uses MiniMagick to extract EXIF metadata from an image file.
     # By extending ImageAnalyzer, all the error handling is done
-    # for us.
+    # for us:
+    #
+    # - Downloading image to a tempfile if necessary (e.g. remote stores)
+    # - Handling an invalid image/a file format not supported by MiniMagick
+    # - Handling MiniMagick not being installed
+    #
+    # https://github.com/rails/rails/blob/master/activestorage/lib/active_storage/analyzer/image_analyzer.rb
     class Analyzer < ActiveStorage::Analyzer::ImageAnalyzer
       def metadata
         { exif: read_image(&:exif) }
